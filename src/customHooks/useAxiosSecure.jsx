ca169a6e63f50c613a9e542417fc8a62ac30d327
mehedi-hasannnn <mhasan2999@gmail.com>
2025-05-24 11:15:26 +0600

    custom hooks designed with axios public and secure

---
+++ src/customHooks/useAxiosSecure.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}`,
+});
+
+export default function useAxiosSecure() {
+  const navigate = useNavigate();
+  const { logoutUser } = useAuth();
+  // request interceptor to add authorization header for every secure API
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logoutUser();
+        navigate("/auth/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+  return axiosSecure;
+}
